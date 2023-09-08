e9f6e391b4f4fdc08e40c26e0d2074cbcc5abd1e
Ian-Kimeli <iankimeli014@gmail.com>
2023-09-08 14:46:36 -0700

    File /**

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ *Return: alwats 0
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar (d);
+			d++;
+		}
+	purchar('\n');
+	return (0);
+}
