c623997920d737bf84a300180ab7b9ee159cbca0
Ian-Kimeli <iankimeli014@gmail.com>
2023-09-08 15:58:37 -0700

    File 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ *Return: alwats 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (n = 97; n <= 102; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
