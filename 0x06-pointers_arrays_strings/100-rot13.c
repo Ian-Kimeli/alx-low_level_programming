0c6dde6a147ba245208a2c1fb37e3cd155c9116d
Ian-Kimeli <iankimeli014@gmail.com>
2023-09-23 05:04:08 -0700

    File 100-rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * @rot13: Shifts each letter of string b 13 places in the alphabet
+ * 
+ * Return: Always 0.
+ */
+char *rot13(char *s)
+{
+	char *ptr = s;
+
+	while (*s)
+	{
+		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'z'))
+		{
+			char base = (*s >= 'a' && *s <= 'z') ? 'a' : 'A'; *s = ((*s - base + 13) % 26) + base;
+		}
+		s++;
+	}
+	return (ptr);
+}
