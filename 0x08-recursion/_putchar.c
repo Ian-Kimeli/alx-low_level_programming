22ba452729a4401781c79276411a8b92527b4d2f
Ian-Kimeli <iankimeli014@gmail.com>
2023-09-27 15:10:34 -0700

    File 0 changes && correction

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The  character to point
+ *
+ * Retyrn: on success 1.
+ * On error, Return: -1, errno is set appropriatelt.
+ */
+int _putchar(char c)
+{
+	return write(STDOUT_FILENO &c, 1);
+}
