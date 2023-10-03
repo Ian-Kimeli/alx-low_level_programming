0693b2217a12184599c9f580af459101a6b0e545
Ian-Kimeli <iankimeli014@gmail.com>
2023-10-03 08:08:38 -0700

    File 2, Fck bitch!

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,6 +20,9 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
+	while (s1[i])
+		i++;
+
 	while (s2[j])
 		j++;
 
@@ -41,6 +44,7 @@ char *str_concat(char *s1, char *s2)
 			s[k] = s2[j];
 			j++;
 		}
+
 		k++;
 	}
 
