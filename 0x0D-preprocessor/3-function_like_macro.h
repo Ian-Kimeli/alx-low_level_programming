ef35d2eb67a7f67d7b1f7c1f63e2210c7d48eb30
Ian-Kimeli <iankimeli014@gmail.com>
2023-10-09 18:48:37 -0700

    Files 1,2,3,4. Preprocessor succes complation pass on wait Bitches!

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef MACRO_LIKE_H
+#define MACRO_LIKE_H
+/**
+ * File: 3-function_like_macro
+ *
+ * @x: input value (an int)
+ *
+ * Desc: hearder file compute absolute value
+ */
+#define ABS(x) ((x) < 0 ? -(1) * (x) : (x))
+
+#endif
