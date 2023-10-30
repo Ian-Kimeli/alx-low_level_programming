2a9b42b3680994452b5a1f41a3190b94b7f76f45
Ian-Kimeli <iankimeli014@gmail.com>
2023-10-30 16:40:16 -0700

    File 3 cp incitatous

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,26 +33,27 @@ void error_file(int file_from, int file_to, char *argv[])
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
-	ssize_t nchars, nwr;
+	ssize_t nchar, nwr;
 	char buf[1024];
 
-	if (argc != 1)
+	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
+
 	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
-	nchars = 1024;
-	while (nchars == 1024)
+	nchar = 1024;
+	while (nchar == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
-		if (nchars == -1)
+		nchar = read(file_from, buf, 1024);
+		if (nchar == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
-		if (nwr == -1)
+		nwr = write(file_to, buf, 1024);
+			if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
@@ -62,11 +63,9 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
-
 	err_close = close(file_to);
-	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to);
 		exit(100);
 	}
 	return (0);
