a73508669add47314342d09eedb7b3accc53fa56
Ian-Kimeli <iankimeli014@gmail.com>
2023-12-13 04:40:38 -0800

    Files 1, 2, 3 all done

---
+++ 0x18-dynamic_libraries/all.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ * div - divideds two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: divided result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: addition
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: subtracted result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * add - add two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: addition
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * mod - mod two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: final answer
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/div.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * div - divideds two integers
+ * @a: first int
+ * @b: second int
+ *
+ * Return: divided result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
